c6d86f14e6d5ba03c322137c9cc45ac041e929dc
Pablo Chesini <pchesini1@gmail.com>
2024-10-11 11:48:06 -0300

    Implementacion de la capa repository para Family

--- backend/hr/src/main/java/com/miksa/hr/repository/IFamilyRepository.java
+++ backend/hr/src/main/java/com/miksa/hr/repository/IFamilyRepository.java
@@ -1,4 +1,19 @@
 package com.miksa.hr.repository;
 
-public interface IFamilyRepository {
+import com.miksa.hr.entity.Family;
+import com.miksa.hr.entity.enums.FamilyRelation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface IFamilyRepository extends JpaRepository<Family, Long> {
+    //para buscar por cumpleaños
+    List<Family> findByDateOfBirth(LocalDate birthDate);
+    //para buscar por relacion (madre padre hijo...)
+    List<Family> findByRelation(FamilyRelation relation);
+
+    List<Family> findByEmployeesId(Long id);
 }
