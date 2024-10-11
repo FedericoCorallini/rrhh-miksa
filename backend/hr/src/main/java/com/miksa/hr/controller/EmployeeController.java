f7f08fc2d911d9a45678bd13904fbe8cbd56fa88
Pablo Chesini <pchesini1@gmail.com>
2024-10-11 11:49:51 -0300

    fix

--- backend/hr/src/main/java/com/miksa/hr/controller/EmployeeController.java
+++ backend/hr/src/main/java/com/miksa/hr/controller/EmployeeController.java
@@ -17,7 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/employee")
+@RequestMapping("/api/family")
 @CrossOrigin
 public class EmployeeController {
 
