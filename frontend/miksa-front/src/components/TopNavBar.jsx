43c4f0f9622d681138aff062d6028b626071129d
damiialvite <dalvite@alu.frlp.utn.edu.ar>
2024-11-06 15:26:18 -0300

    Previo a validacion

--- frontend/miksa-front/src/components/TopNavBar.jsx
+++ frontend/miksa-front/src/components/TopNavBar.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import { NavLink } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import LogoutButton from './LogoutButton';
 
 function TopNavBar() {
   return (
-    <Navbar bg="light" data-bs-theme="light">
-      <Container>
-        <Nav className="me-auto">
-          <Nav.Link as={NavLink} to='/perfil/0'>Perfil</Nav.Link>
-          <Nav.Link as={NavLink} to='/solicitudes'>Solicitudes</Nav.Link>
-          <Nav.Link as={NavLink} to='/empleados'>Empleados</Nav.Link>
-          <Nav.Link as={NavLink} to='/permisos'>Permisos</Nav.Link>
-        </Nav>
-      </Container>
-      <LogoutButton />
-    </Navbar>
+    <AppBar position="static" color="default">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Button color="inherit" component={NavLink} to='/perfil/0'>Perfil</Button>
+          <Button color="inherit" component={NavLink} to='/solicitudes'>Solicitudes</Button>
+          <Button color="inherit" component={NavLink} to='/empleados'>Empleados</Button>
+          <Button color="inherit" component={NavLink} to='/permisos'>Permisos</Button>
+        </Typography>
+        <LogoutButton />
+      </Toolbar>
+    </AppBar>
   );
 }
 
