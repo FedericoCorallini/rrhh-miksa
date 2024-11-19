db679e990125fd6db056a1962a3dd6ac854d14dd
Santiago <santicanu4@gmail.com>
2024-11-19 15:51:13 -0300

    Agregue las validaciones del perfil

--- frontend/miksa-front/src/components/profile/ProfileForm.jsx
+++ frontend/miksa-front/src/components/profile/ProfileForm.jsx
@@ -1,19 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
 import { BasicDatePicker } from "../BasicDatePicker";
 import { Button } from "@mui/material";
 import { postEmployee, putEmployee } from "../../utils/Axios";
 import { useAuth0 } from "@auth0/auth0-react";
 
+// Componente reutilizable para TextField
+const FormField = ({ label, name, rules, register, errors, select, children, ...props }) => (
+  <TextField
+    label={label}
+    {...register(name, rules)}
+    error={!!errors[name]}
+    helperText={errors[name]?.message}
+    select={select}
+    variant="standard"
+    fullWidth
+    {...props}
+  >
+    {children}
+  </TextField>
+);
+
 export const ProfileForm = ({ profile }) => {
-  const { register, handleSubmit, setValue, formState: { errors } } = useForm({
+  const { register, handleSubmit, setValue, watch, formState: { errors } } = useForm({
     defaultValues: profile,
   });
 
   const { user } = useAuth0();
+  const [loading, setLoading] = useState(false);
+  const [snackbar, setSnackbar] = useState({ open: false, message: "", severity: "" });
 
   useEffect(() => {
     if (profile) {
@@ -23,11 +43,22 @@ export const ProfileForm = ({ profile }) => {
     }
   }, [profile, setValue]);
 
+  const handleSnackbarClose = () => setSnackbar({ ...snackbar, open: false });
+
   const onSubmit = async (data) => {
-    if (data.id === undefined) {
-      await postEmployee(data);
-    } else {
-      await putEmployee(data.id, data);
+    setLoading(true);
+    try {
+      if (data.id === undefined) {
+        await postEmployee(data);
+      } else {
+        await putEmployee(data.id, data);
+      }
+      setSnackbar({ open: true, message: "Datos guardados exitosamente", severity: "success" });
+    } catch (error) {
+      console.error(error);
+      setSnackbar({ open: true, message: "Error al guardar los datos", severity: "error" });
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -35,135 +66,157 @@ export const ProfileForm = ({ profile }) => {
     <Box component="form" onSubmit={handleSubmit(onSubmit)}>
       <Box
         sx={{
-          "& > :not(style)": { m: 1, width: "25ch" },
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+          gap: 2,
         }}
-        noValidate
         autoComplete="off"
       >
-        <TextField
+        <FormField
           label="Nombre"
-          variant="standard"
-          {...register("firstname", { required: "El nombre es obligatorio" })}
-          error={!!errors.firstname}
-          helperText={errors.firstname?.message}
+          name="firstname"
+          rules={{ required: "El nombre es obligatorio" }}
+          register={register}
+          errors={errors}
         />
-        <TextField
+        <FormField
           label="Apellido"
-          variant="standard"
-          {...register("lastname", { required: "El apellido es obligatorio" })}
-          error={!!errors.lastname}
-          helperText={errors.lastname?.message}
+          name="lastname"
+          rules={{ required: "El apellido es obligatorio" }}
+          register={register}
+          errors={errors}
         />
-        <TextField
+        <FormField
           label="DNI"
-          variant="standard"
-          {...register("dni", { required: "El DNI es obligatorio" })}
-          error={!!errors.dni}
-          helperText={errors.dni?.message}
+          name="dni"
+          rules={{ required: "El DNI es obligatorio" }}
+          register={register}
+          errors={errors}
         />
-        <TextField
+        <FormField
           label="CUIL"
-          variant="standard"
-          {...register("cuil", { required: "El CUIL es obligatorio" })}
-          error={!!errors.cuil}
-          helperText={errors.cuil?.message}
+          name="cuil"
+          rules={{ required: "El CUIL es obligatorio" }}
+          register={register}
+          errors={errors}
         />
-        <TextField
+        <FormField
           label="Celular"
-          variant="standard"
-          {...register("cell_phone_number", { required: "El celular es obligatorio" })}
-          error={!!errors.cell_phone_number}
-          helperText={errors.cell_phone_number?.message}
+          name="cell_phone_number"
+          rules={{ required: "El celular es obligatorio" }}
+          register={register}
+          errors={errors}
         />
-        <TextField
+        <FormField
           label="Teléfono"
-          variant="standard"
-          {...register("home_phone_number", { required: "El teléfono es obligatorio" })}
-          error={!!errors.home_phone_number}
-          helperText={errors.home_phone_number?.message}
+          name="home_phone_number"
+          rules={{ required: "El teléfono es obligatorio" }}
+          register={register}
+          errors={errors}
         />
-        <TextField
+        <FormField
           label="Email"
-          variant="standard"
-          {...register("email", {
+          name="email"
+          rules={{
             required: "El email es obligatorio",
             pattern: { value: /\S+@\S+\.\S+/, message: "El email no es válido" },
-          })}
-          error={!!errors.email}
-          helperText={errors.email?.message}
+          }}
+          register={register}
+          errors={errors}
         />
-        <TextField
+        <FormField
           label="Estado civil"
-          variant="standard"
+          name="marital_status"
+          rules={{ required: "El estado civil es obligatorio" }}
+          register={register}
+          errors={errors}
           select
-          {...register("marital_status", { required: "El estado civil es obligatorio" })}
-          error={!!errors.marital_status}
-          helperText={errors.marital_status?.message}
         >
           <MenuItem value="soltero">Soltero</MenuItem>
           <MenuItem value="casado">Casado</MenuItem>
           <MenuItem value="divorciado">Divorciado</MenuItem>
           <MenuItem value="viudo">Viudo</MenuItem>
-        </TextField>
-        <TextField
+        </FormField>
+        <FormField
           label="Género"
-          variant="standard"
+          name="gender"
+          rules={{ required: "El género es obligatorio" }}
+          register={register}
+          errors={errors}
           select
-          {...register("gender", { required: "El género es obligatorio" })}
-          error={!!errors.gender}
-          helperText={errors.gender?.message}
         >
           <MenuItem value="hombre">Hombre</MenuItem>
           <MenuItem value="mujer">Mujer</MenuItem>
           <MenuItem value="otro">Otro</MenuItem>
-        </TextField>
-        <TextField
+        </FormField>
+        <FormField
           label="Nacionalidad"
-          variant="standard"
+          name="nationality"
+          rules={{ required: "La nacionalidad es obligatoria" }}
+          register={register}
+          errors={errors}
           select
-          {...register("nationality", { required: "La nacionalidad es obligatoria" })}
-          error={!!errors.nationality}
-          helperText={errors.nationality?.message}
         >
           <MenuItem value="Argentina">Argentina</MenuItem>
           <MenuItem value="Brasil">Brasil</MenuItem>
           <MenuItem value="Chile">Chile</MenuItem>
-          {/* Agrega más países latinoamericanos aquí */}
-        </TextField>
-        <TextField
+        </FormField>
+        <FormField
           label="Horario laboral"
-          variant="standard"
-          {...register("working_hours", { required: "El horario laboral es obligatorio" })}
-          error={!!errors.working_hours}
-          helperText={errors.working_hours?.message}
+          name="working_hours"
+          rules={{ required: "El horario laboral es obligatorio" }}
+          register={register}
+          errors={errors}
         />
-        <TextField
+        <FormField
           label="Puesto laboral"
-          variant="standard"
-          {...register("job_position", { required: "El puesto laboral es obligatorio" })}
-          error={!!errors.job_position}
-          helperText={errors.job_position?.message}
+          name="job_position"
+          rules={{ required: "El puesto laboral es obligatorio" }}
+          register={register}
+          errors={errors}
         />
         <BasicDatePicker
           label="Fecha de nacimiento"
-          {...register("date_of_birth", { required: "La fecha de nacimiento es obligatoria" })}
+          value={watch("date_of_birth")}
+          onChange={(newValue) => setValue("date_of_birth", newValue, { shouldValidate: true })}
         />
         <BasicDatePicker
           label="Fecha de admisión"
-          {...register("date_of_admission")}
+          value={watch("date_of_admission")}
+          onChange={(newValue) => setValue("date_of_admission", newValue, { shouldValidate: true })}
         />
       </Box>
-
       {user && user["roles/roles"] && user["roles/roles"].includes("admin") && (
         <Button
           sx={{ mt: "15px" }}
           type="submit"
           variant="contained"
           size="large"
+          disabled={loading}
         >
-          Guardar
+          {loading ? "Guardando..." : "Guardar"}
         </Button>
       )}
+
+      {/* Ventana emergente (Snackbar) */}
+      <Snackbar
+        open={snackbar.open}
+        autoHideDuration={5000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }} // Posición en la parte superior central
+      >
+        <Alert
+          onClose={handleSnackbarClose}
+          severity={snackbar.severity}
+          sx={{
+            width: "100%",
+            maxWidth: "400px", // Opcional: limitar el ancho del mensaje
+          }}
+        >
+          {snackbar.message}
+        </Alert>
+      </Snackbar>
+
     </Box>
   );
 };
