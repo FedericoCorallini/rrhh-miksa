74b765fcda742cc4050906deae00cd5e61e77cdc
damiialvite <dalvite@alu.frlp.utn.edu.ar>
2025-01-21 17:07:14 -0300

    Estilos

--- frontend/miksa-front/src/components/TopNavBar.jsx
+++ frontend/miksa-front/src/components/TopNavBar.jsx
@@ -5,16 +5,23 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './LogoutButton';
+import { useAuth0 } from '@auth0/auth0-react';
+import './TopNavBar.css';
 
 function TopNavBar() {
+  const { user } = useAuth0();
   return (
     <AppBar position="static" color="default">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           <Button color="inherit" component={NavLink} to='/perfil/0'>Perfil</Button>
           <Button color="inherit" component={NavLink} to='/solicitudes'>Solicitudes</Button>
-          <Button color="inherit" component={NavLink} to='/empleados'>Empleados</Button>
-          <Button color="inherit" component={NavLink} to='/permisos'>Permisos</Button>
+          {user && user['roles/roles'] && user['roles/roles'].includes('admin') && (
+            <Button color="inherit" component={NavLink} to='/empleados'>Empleados</Button>
+          )}
+          {user && user['roles/roles'] && (user['roles/roles'].includes('admin') || user['roles/roles'].includes('gerente')) && (
+            <Button color="inherit" component={NavLink} to='/permisos'>Permisos</Button>
+          )}
         </Typography>
         <LogoutButton />
       </Toolbar>
--- frontend/miksa-front/src/pages/ProfilePage.jsx
+++ frontend/miksa-front/src/pages/ProfilePage.jsx
@@ -69,7 +69,7 @@ export const ProfilePage = () => {
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Perfil" {...a11yProps(0)} />
+          <Tab label="Datos personales" {...a11yProps(0)} />
           <Tab label="Datos bancarios" {...a11yProps(1)} />
           <Tab label="Grupo familiar" {...a11yProps(2)} />
           <Tab label="Documentacion" {...a11yProps(3)} />
