f616c08dbba6120f923deb4e8d22dea7ae40010c
damiialvite <dalvite@alu.frlp.utn.edu.ar>
2025-02-07 08:48:10 -0300

    PermissionDetails mejorado

--- frontend/miksa-front/src/components/PermissionDetails.jsx
+++ frontend/miksa-front/src/components/PermissionDetails.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography, Grid } from "@mui/material";
 
 export const PermissionDetailsModal = ({ permission, onClose }) => {
   return (
@@ -7,41 +7,89 @@ export const PermissionDetailsModal = ({ permission, onClose }) => {
       <Typography variant="h6" gutterBottom align="center" sx={{ color: 'darkslategray' }}>
         Detalles de la Solicitud
       </Typography>
-      <TextField
-        label="Empleado"
-        value={permission.employee_name}
-        fullWidth
-        margin="normal"
-        InputProps={{ readOnly: true, style: { color: 'gray' } }}
-      />
-      <TextField
-        label="Motivo"
-        value={permission.reason}
-        fullWidth
-        margin="normal"
-        InputProps={{ readOnly: true, style: { color: 'gray' } }}
-      />
-      <TextField
-        label="Fecha de inicio"
-        value={permission.start_date}
-        fullWidth
-        margin="normal"
-        InputProps={{ readOnly: true, style: { color: 'gray' } }}
-      />
-      <TextField
-        label="Fecha de finalización"
-        value={permission.end_date}
-        fullWidth
-        margin="normal"
-        InputProps={{ readOnly: true, style: { color: 'gray' } }}
-      />
-      <TextField
-        label="Estado"
-        value={permission.permission_state}
-        fullWidth
-        margin="normal"
-        InputProps={{ readOnly: true, style: { color: 'gray' } }}
-      />
+      <Grid container columnSpacing={2} rowSpacing={0}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Empleado"
+            value={permission.employee_name}
+            fullWidth
+            margin="normal"
+            InputProps={{ readOnly: true, style: { color: 'gray' } }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Puesto laboral"
+            value={permission.employee_position}
+            fullWidth
+            margin="normal"
+            InputProps={{ readOnly: true, style: { color: 'gray' } }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Motivo"
+            value={permission.reason}
+            fullWidth
+            margin="normal"
+            InputProps={{ readOnly: true, style: { color: 'gray' } }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Detalles"
+            value={permission.details}
+            fullWidth
+            margin="normal"
+            InputProps={{ readOnly: true, style: { color: 'gray' } }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Fecha de inicio"
+            value={permission.start_date}
+            fullWidth
+            margin="normal"
+            InputProps={{ readOnly: true, style: { color: 'gray' } }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Fecha de finalización"
+            value={permission.end_date}
+            fullWidth
+            margin="normal"
+            InputProps={{ readOnly: true, style: { color: 'gray' } }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Hora de inicio"
+            value={permission.start_time}
+            fullWidth
+            margin="normal"
+            InputProps={{ readOnly: true, style: { color: 'gray' } }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Hora de finalización"
+            value={permission.end_time}
+            fullWidth
+            margin="normal"
+            InputProps={{ readOnly: true, style: { color: 'gray' } }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Estado"
+            value={permission.permission_state}
+            fullWidth
+            margin="normal"
+            InputProps={{ readOnly: true, style: { color: 'gray' } }}
+          />
+        </Grid>
+      </Grid>
       <Button variant="contained" color="primary" onClick={onClose} sx={{ mt: 2 }}>
         Cerrar
       </Button>
