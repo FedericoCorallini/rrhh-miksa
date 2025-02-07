8de5cae9b613a1c8b761d105d3ee8341560c2902
damiialvite <dalvite@alu.frlp.utn.edu.ar>
2025-02-07 09:22:31 -0300

    ...

--- frontend/miksa-front/src/pages/PermissionRequestPage.jsx
+++ frontend/miksa-front/src/pages/PermissionRequestPage.jsx
@@ -2,18 +2,41 @@ import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { deletePermission, getEmployee, getFile } from "../utils/Axios";
-import { PermissionRequestModalForm} from "../components/PermissionRequestModalForm.jsx";
-import Button from '@mui/material/Button'; // Importar Button
-import Modal from '@mui/material/Modal'; // Importar Modal
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline'; // Importar icono de Delete
-import AddIcon from '@mui/icons-material/Add'; // Importar icono de Add
-import AttachFileIcon from '@mui/icons-material/AttachFile'; // Importar icono de AttachFile
+import { PermissionRequestModalForm } from "../components/PermissionRequestModalForm.jsx";
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import AddIcon from '@mui/icons-material/Add';
+import CloudUpload from '@mui/icons-material/CloudUpload';
+import CloudDownload from '@mui/icons-material/CloudDownload';
+import { DocumentationModalForm } from "../components/DocumentationModalForm.jsx";
 
-
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'background.paper',
+  border: '1px solid #000',
+  boxShadow: 24,
+  p: 4,
+  minWidth: '40%',
+  maxWidth: '70%',
+};
 
 export const PermissionRequestPage = () => {
+  const [permissions, setPermissions] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [openDocumentationModal, setOpenDocumentationModal] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleOpenDocumentationModal = () => setOpenDocumentationModal(true);
+  const handleCloseDocumentationModal = () => setOpenDocumentationModal(false);
+
   const COLUMNS = [
-    { field: "motivo", headerName: "Motivo", width: 200 }, 
+    { field: "reason", headerName: "Motivo", width: 200 },
     { field: "details", headerName: "Detalles", width: 200 },
     {
       field: "start_date",
@@ -21,7 +44,6 @@ export const PermissionRequestPage = () => {
       width: 180,
       type: "Date",
     },
-    
     {
       field: "end_date",
       headerName: "Fecha de finalizacion",
@@ -47,12 +69,20 @@ export const PermissionRequestPage = () => {
       width: 350,
       renderCell: (params) => (
         <>
-        <Button variant="outlined" color="primary" onClick={() => downloadFile(params.row.documentation.id)}>
-        <AttachFileIcon fontSize="small"/>
-      </Button>
-          <Button variant="outlined" color="error" onClick={() => deleteRow(params.row.id)}>
-            <DeleteOutlineIcon fontSize="small"/>
-          </Button>
+          {params.row.documentation ? (
+            <Button variant="outlined" color="primary" onClick={() => downloadFile(params.row.documentation.id)}>
+              <CloudDownload fontSize="small" />
+            </Button>
+          ) : (
+            <Button variant="outlined" color="primary" onClick={handleOpenDocumentationModal}>
+              <CloudUpload fontSize="small" />
+            </Button>
+          )}
+          {params.row.permission_state === "PENDIENTE" && (
+            <Button variant="outlined" color="error" onClick={() => deleteRow(params.row.id)}>
+              <DeleteOutlineIcon fontSize="small" />
+            </Button>
+          )}
         </>
       ),
     },
@@ -69,23 +99,16 @@ export const PermissionRequestPage = () => {
 
   const downloadFile = async (id) => {
     const data = await getFile(id);
-    const pdfBlob = new Blob([data], { type: 'application/pdf' })
+    const pdfBlob = new Blob([data], { type: 'application/pdf' });
     const url = window.URL.createObjectURL(pdfBlob);
     window.open(url, '_blank');
   };
 
-
-  const [permissions, setPermissions] = useState([]);
-
   const callApi = async () => {
     const respuesta = await getEmployeeByEmail();
     setPermissions(respuesta.data.absence_permissions_list);
-    sessionStorage.setItem('employeeId', respuesta.data.id)
+    sessionStorage.setItem('employeeId', respuesta.data.id);
   };
-
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
 
   return (
     <Box sx={{ height: 450, width: 1 }}>
@@ -96,7 +119,6 @@ export const PermissionRequestPage = () => {
         disableDensitySelector
         disableColumnFilter
         slots={{ toolbar: GridToolbar }}
-   
         initialState={{
           sorting: {
             sortModel: [{ field: 'start_date_time', sort: 'desc' }],
@@ -112,7 +134,7 @@ export const PermissionRequestPage = () => {
           marginTop: "2%",
           marginLeft: "1%",
           '&:hover': {
-            backgroundColor: "#4caf50", // Verde más oscuro
+            backgroundColor: "#4caf50",
           },
         }}
         onClick={handleOpen}
@@ -125,22 +147,22 @@ export const PermissionRequestPage = () => {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          minWidth: '50%',
-          maxWidth: '70%',
-          bgcolor: 'background.paper',
-          border: '1px solid #000',
-          boxShadow: 24,
-          p: 4,
-        }}>
+        <Box sx={style}>
           <PermissionRequestModalForm />
+        </Box>
+      </Modal>
+      <Modal
+        open={openDocumentationModal}
+        onClose={handleCloseDocumentationModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <DocumentationModalForm employeeId={sessionStorage.getItem('employeeId')} handleClose={handleCloseDocumentationModal} reload={callApi} fromPRPage={true} />
         </Box>
       </Modal>
     </Box>
   );
 };
+
 export default PermissionRequestPage;
